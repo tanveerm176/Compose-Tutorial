8cd91e2caec194c3856225b8af9f9b9ed97b93af
tanveerm176 <tanveerm176@gmail.com>
2023-10-21 20:16:48 -0400

    added sample glossary scroll list

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,15 +35,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             /*MessageCard("Android")*/
-            /*
-            //Used with Message data class defined below
+            /*//Used with Message data class defined below
             MessageCard(msg = Message("Android", "JetPackCompose"))
-            */
-
+*/
             //using provided theme to style the message
             ComposeTutorialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(msg = Message("Muhammad Tanveer", "Capstone Project"))
+                    MessageCard(msg = Message("Fixed Income",
+                        "a type of investment that pays the investor a fixed amount on a fixed schedule"))
                 }
             }
 
@@ -51,22 +52,29 @@ class MainActivity : ComponentActivity() {
 }
 
 
-/*//func passed in name and configs text elmnt
-@Composable
+//func passed in name and configs text elmnt
+/*@Composable
 fun MessageCard(name: String){
     Text(text = "Hello, $name!")
+}*/
+
+/*@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(Message("Android", "JetPackCompose"))
 }*/
 
 //create a class that holds author and body to be displayed
 data class Message(val author: String, val body: String)
 
 
-/*
 //Display without column
-@Composable
+/*@Composable
 fun MessageCard(msg: Message){
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
 }*/
 
 /*//Display with Column
@@ -81,17 +89,31 @@ fun MessageCard(msg: Message){
 /*//Display with Row Img and Column text with no layout config
 @Composable
 fun MessageCard(msg: Message){
-    Row{
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Profile Picture"
-        )
-    }
 
     Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Profile Picture"
+            )
+            Column {
+                Text(text = msg.author)
+                Text(text = msg.body)
+            }
+        }
+
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Profile Picture"
+            )
+            Column {
+                Text(text = msg.author)
+                Text(text = msg.body)
+            }
+        }
     }
+
 }*/
 
 /*//Display with Row Img and Column text with layout padding and config
@@ -172,7 +194,7 @@ fun MessageCard(msg: Message) {
 //The Preview Component is separate
 // from the component displayed on the screen
 /*@Preview*/
-@Preview(name = "Light Mode", heightDp = 100)
+/*@Preview(name = "Light Mode", heightDp = 100)
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
@@ -181,12 +203,12 @@ fun MessageCard(msg: Message) {
 )
 @Composable
 fun PreviewMessageCard() {
-    /*MessageCard(name = "Android")*/
-    /*
+    *//*MessageCard(name = "Android")*//*
+    *//*
     //Used with Message data class defined above
     MessageCard(
         msg = Message("Android", "JetPackCompose")
-    )*/
+    )*//*
 
     //using provided theme to style the message
     ComposeTutorialTheme {
@@ -194,6 +216,84 @@ fun PreviewMessageCard() {
             MessageCard(msg = Message("Android", "JetPackCompose"))
         }
     }
+}*/
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Fixed Income",
+            "a type of investment that pays the investor a fixed amount on a fixed schedule".trim()
+        ),
+        Message(
+            "Bond",
+            """In finance, a bond is a type of security under which the issuer owes the holder a debt, and is obliged – depending on the terms – to provide cash flow to the creditor.""".trim()
+        ),
+        Message(
+            "Roth IRA",
+            """A Roth IRA is an individual retirement account under United States law that is generally not taxed upon distribution, provided certain conditions are met.""".trim()
+        ),
+        Message(
+            "Vehicles",
+            "a financial account or product used to create returns. In other words, it generally refers to any container investors use to grow their money. This includes individual securities such as stocks and bonds or pooled investments like mutual funds and ETFS.".trim()
+        ),
+        Message(
+            "Mutual Fund",
+            """A mutual fund is a type of investment vehicle consisting of a portfolio of stocks, bonds, or other securities. It gives small or individual investors access to diversified, professionally managed portfolios. Instead of investing in a single stock or bond, by purchasing shares of a mutual fund, the investor can reap the benefits of a diversified portfolio.""".trim()
+        ),
+        Message(
+            "ETF (Exchange-Traded Fund)",
+            "a type of pooled investment security that operates much like a mutual fund, but unlike mutual funds, ETFs can be purchased or sold on a stock exchange the same way that a regular stock can. This means that ETF share prices fluctuate all day as the ETF is bought and sold; this is different from mutual funds, which only trade once a day after the market closes.  ETFs generally also have less fees associated with it than mutual funds.".trim()
+        ),
+        Message(
+            "Bond",
+            "A bond is a debt security, which means borrowers issue bonds to raise money from investors willing to lend them money for a certain amount of time. When you buy a bond, you are lending to the issuer, which may be a government, municipality, or corporation. In return, the issuer promises to pay you a specified rate of interest during the life of the bond and to repay the principal, also known as face value or par value of the bond, when it \"matures,\" or comes due after a set period of time.".trim()
+        ),
+        Message(
+            "Coupon Payment",
+            "A coupon or coupon payment is the annual interest rate paid on a bond.".trim()
+        ),
+        Message(
+            "Principal",
+            "The principal refers to the face value of the bond, that is, the money the investor lent to the bond issuer, which the issuer promises to pay back at the maturity date.".trim()
+        ),
+        Message(
+            "Maturity Date",
+            "the date on which the final payment is due on the bond, at which point the principal (and all remaining interest) is due to be paid.".trim()
+        ),
+        Message(
+            "Credit Risk",
+            "Credit risk refers to the risk that a lender may not receive the owed principal and interest. This is an unsystematic risk, which means this risk is often specific to an individual company or market, and thus can be mitigated by diversifying one’s portfolio.".trim()
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
 }
 
 
